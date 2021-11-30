d26ce628f2fe81c49e6e1007ef2f28e5a86c5d4d
ctile <90358537+ctile@users.noreply.github.com>
2021-11-30 14:39:09 -0500

    stylesheets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import "./styles2.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./home/Home.js";
 import Trails from "./home/trails/Trails.js";
